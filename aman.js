f1213cd4935486b996ad265ad51b42eb49043c7c
pathakaman <aman.kumar@diatoz.com>
2020-02-27 13:17:39 +0530

    changes

---
+++ aman.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class Aman extends React.component {
+    render() {
+        return (
+            <div>
+                <p>Heading</p>
+            </div>
+        )
+    }
+}
